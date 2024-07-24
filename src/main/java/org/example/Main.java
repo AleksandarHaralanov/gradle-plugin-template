f520c835a368b0d6dba2894c5fe5f0fcaee21c08
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2024-07-24 20:10:32 +0300

    Small fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static org.bukkit.Bukkit.getLogger;
-
 public class Main extends JavaPlugin {
 
     private static Main instance;
@@ -14,14 +12,14 @@ public class Main extends JavaPlugin {
 
         // Initialization logic (e.g., register listeners, commands)
 
-        getLogger().info(String.format("[%s] v%s Enabled.", getDescription().getName(), getDescription().getVersion()));
+        getServer().getLogger().info(String.format("[%s] v%s Enabled.", getDescription().getName(), getDescription().getVersion()));
     }
 
     @Override
     public void onDisable() {
         // Cleanup logic (e.g., save data, unregister listeners)
 
-        getLogger().info(String.format("[%s] v%s Disabled.", getDescription().getName(), getDescription().getName()));
+        getServer().getLogger().info(String.format("[%s] v%s Disabled.", getDescription().getName(), getDescription().getName()));
     }
 
     public static Main getInstance() {
